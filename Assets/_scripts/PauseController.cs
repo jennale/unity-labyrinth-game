9816ecbe206759a5ecda64a8bd17dee15dd13eb6
Le, Jenna <jenna.le@londonlife.com>
2017-10-14 03:54:01 -0400

    added pause menu

--- Assets/_scripts/PauseController.cs
+++ Assets/_scripts/PauseController.cs
@@ -10,24 +10,44 @@ public class PauseController : MonoBehaviour {
 	void Start () {
         Button exit = GameObject.Find("ExitButton").GetComponent<Button>();
         Button restart = GameObject.Find("RestartButton").GetComponent<Button>();
+        Button unpause = GameObject.Find("UnpauseButton").GetComponent<Button>();
+        Button pause = GameObject.Find("PauseButton").GetComponent<Button>();
 
         exit.onClick.AddListener(ReturnToStart);
-
         restart.onClick.AddListener(RestartGame);
+        unpause.onClick.AddListener(ContinueGame);
+        pause.onClick.AddListener(PauseGame);
 
-        //gameObject.SetActive(false);
+        gameObject.SetActive(false);
 	}
 
-    void RestartGame() {
-        SceneManager.LoadScene("Labyrinth");
-    }
-
-    void ReturnToStart() {
-        SceneManager.LoadScene("Start");
-    }
-	
 	// Update is called once per frame
 	void Update () {
-		
+
 	}
+
+    void togglePausePanel () {
+        gameObject.SetActive(!gameObject.activeSelf);
+    }
+
+
+    void PauseGame() {
+        Time.timeScale = 0;
+        togglePausePanel();
+    }
+
+    void ContinueGame() {
+        Time.timeScale = 1;
+        togglePausePanel();
+    }
+
+	void RestartGame() {
+        Time.timeScale = 1;
+		SceneManager.LoadScene("Labyrinth");
+	}
+
+    void ReturnToStart() {
+        Time.timeScale = 1;
+        SceneManager.LoadScene("Start");
+    }
 }
