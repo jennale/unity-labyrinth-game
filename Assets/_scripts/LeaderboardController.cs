2a3a1b1eff258fefb9c43b1dae41501eee1de4ed
Le, Jenna <jenna.le@londonlife.com>
2017-10-14 01:14:00 -0400

    Cleanups and comments

--- Assets/_scripts/LeaderboardController.cs
+++ Assets/_scripts/LeaderboardController.cs
@@ -3,84 +3,87 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+/// <summary>
+/// Controls the Leaderboard and Leaderboard screen 
+/// </summary>
 public class LeaderboardController : MonoBehaviour {
-    
+    //The leaderboard list
 	static SortedList leaderboard;
+
+    /// <summary>
+    /// The max number of scores to keep in the leaderboard
+    /// </summary>
+    private const int MAX_NUM_RECORDS = 10;
+
+    //Buttons
 	private Button backButton;
 	private Button showButton;
-
+	
+    //Text UI objects
 	private Text leaderboardNames;
 	private Text leaderboardNumbers;
-    private const int MAX_NUM_RECORDS = 10;
 
 	private bool updated = false;
 
 	// Use this for initialization
 	void Start () {
+        //TODO: Replace the leaderboard with UserPreferences if there is one saved.
 		leaderboard = new SortedList ();
 		leaderboardNames = GameObject.Find ("NameList").GetComponent<Text> ();
 		leaderboardNumbers = GameObject.Find ("ScoreList").GetComponent<Text> ();
 
+        //Create button listeners
         backButton = GameObject.Find("BackButton").GetComponent<Button> ();
-        backButton.onClick.AddListener (ToggleLeaderboard);
+		backButton.onClick.AddListener (ToggleLeaderboard);
+        showButton = GameObject.Find("LeaderboardButton").GetComponent<Button> ();
+		showButton.onClick.AddListener(ToggleLeaderboard);
+		
+        //Create leaderboard text 
+        CreateLeaderboard();
 
+        //Test button for adding random scores...
         Button testButton = GameObject.Find("TestButton").GetComponent<Button>();
         testButton.onClick.AddListener(testAdd);
-
-        showButton = GameObject.Find("LeaderboardButton").GetComponent<Button> ();
-        showButton.onClick.AddListener(ToggleLeaderboard);
-
-		CreateLeaderboard();
-
+		
+        //Hide the leaderboard on gamestart
         gameObject.SetActive(false);
 	}
 
 	// Update is called once per frame
 	void Update()
 	{
+        //If the leaderboard was updated, create the new leaderboard text.
 		if (updated)
 		{
 			CreateLeaderboard();
 		}
 	}
 
+    /// <summary>
+    /// Toggles the leaderboard.
+    /// </summary>
     void ToggleLeaderboard() {
         gameObject.SetActive(!gameObject.activeSelf);
     }
 
     /// <summary>
-    /// Hides the leaderboard.
+    /// Generates the Leaderboard text and shows it on the canvas
     /// </summary>
-	void HideLeaderboard () {
-		gameObject.SetActive (false);
-	}
-
-    /// <summary>
-    /// Shows the leaderboard.
-    /// </summary>
-	void ShowLeaderboard () {		
-		gameObject.SetActive (true);
-	}
-
-	void testAdd() {
-        AddToLeaderboard ((int)Random.Range(0.0f, 100.0f), "Username" + leaderboard.Count.ToString());
-	}
-
 	private void CreateLeaderboard () {
         if (leaderboard.Count < 1) {
             leaderboardNames.text = "There are no scores!";
         }
 
-
 		string names = "";
 		string scores = "";
-        int order = 1;
+        int orderLabel = 1;
 
+        //Scores are sorted from lowest to highest scores (reverse for loop)
         for (int i = leaderboard.Count-1; i >= 0; i-- ) {
             Score val = (Score)leaderboard.GetByIndex(i);
-            names += order.ToString() + "\t\t" + val.name + "\n";
+            names += orderLabel.ToString() + "\t\t" + val.name + "\n";
             scores += val.score + "\n";
-			order++; 
+			orderLabel++; 
         }
 
 		leaderboardNames.text = names;
@@ -89,6 +92,12 @@ public class LeaderboardController : MonoBehaviour {
 		updated = false;
 	}
 
+    /// <summary>
+    /// Adds to leaderboard, using a sorted list. The list is sorted by
+    /// the following order: Score (0000XX) > User > Date
+    /// </summary>
+    /// <param name="score">Score integer.</param>
+    /// <param name="user">User.</param>
 	public void AddToLeaderboard(int score, string user) {
         
 		if (leaderboard.Count >= MAX_NUM_RECORDS) {
@@ -109,8 +118,19 @@ public class LeaderboardController : MonoBehaviour {
 
 		updated = true;
 	}
-	
 
+    /// <summary>
+    /// Tests adding to the leaderboard
+    /// </summary>
+	void testAdd()
+	{
+		AddToLeaderboard((int)Random.Range(0.0f, 100.0f), "Username" + leaderboard.Count.ToString());
+	}
+
+
+    /// <summary>
+    /// Score object.
+    /// </summary>
 	internal class Score {
 		public string name;
 		public int score;
--- Assets/_scripts/SplashscreenController.cs
+++ Assets/_scripts/SplashscreenController.cs
@@ -4,27 +4,36 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
+/// <summary>
+/// Controls the initial Start splashscreen
+/// </summary>
 public class SplashscreenController : MonoBehaviour {
 
-    private GameObject starburst;
 	private Button startButton;
+    private GameObject starburst;
 
 	private AssetBundle myLoadedAssetBundle;
 	private string[] scenePaths;
 
-	// Use this for initialization
 	void Start () {
-        starburst = GameObject.Find("Starburst");
+		//Add button listener
 		startButton = GameObject.Find("StartButton").GetComponent<Button>();
 		startButton.onClick.AddListener(StartGameScene);
+
+        //Add animation
+        starburst = GameObject.Find("Starburst");
 	}
 
+    /// <summary>
+    /// Load the main labyrinth game scene.
+    /// </summary>
     private void StartGameScene() {
         SceneManager.LoadScene("Labyrinth");
     }
 	
 	// Update is called once per frame
 	void Update () {
+        //Fun little twirl of the starburst
         starburst.transform.Rotate(new Vector3(0, 0, -2) * Time.deltaTime);
 	}
 }
